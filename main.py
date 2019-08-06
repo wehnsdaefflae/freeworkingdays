d56b1187dd1407fe320688b6f3775e419fbd159d
Mark <wernsdorfer@gmail.com>
2019-08-06 12:49:06 +0200

    small changes

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
 import requests
 import datetime
-from typing import Iterable, Tuple, List, Optional, Union
+from typing import List, Optional, Union
 
-from icalevents.icalevents import events
 from ics import Calendar
-
-
-def dates_from_ics(ical_url: str):
-    return events(ical_url, fix_apple=True)
-
-
-def is_free(day: datetime.datetime) -> bool:
-    return False
-
-
-def get_free_days(free_days: Iterable[datetime.datetime]) -> Tuple[int, int, int, int, int, int, int]:
-    weekdays_off = [0, 0, 0, 0, 0, 0, 0]
-    for each_day in free_days:
-        if is_free(each_day):
-            weekdays_off[each_day] += 1
-    return tuple(weekdays_off)
 
 
 def merge_events(*calendars: Calendar) -> Calendar:
@@ -29,21 +12,22 @@ def merge_events(*calendars: Calendar) -> Calendar:
         each_events = each_calendar.events
         for each_event in each_events:
             c_events.add(each_event)
+
     return calendar
 
 
 def get_calendar_from_url(url: str) -> Calendar:
     ics = requests.get(url)
-    ics_text = ics.text
-    return Calendar(ics_text)
+    return Calendar(ics.text)
 
 
 def most_free_days(calendar: Calendar, time_start: Optional[datetime.date] = None, time_end: Optional[datetime.date] = None) -> List[List[Union[str, int]]]:
     free_weekdays = ["monday", 0], ["tuesday", 0], ["wednesday", 0], ["thursday", 0], ["friday", 0], ["saturday", 0], ["sunday", 0]
 
-    # holidays = sorted(calendar.events, key=lambda _x: _x.begin)
     for each_holiday in calendar.events:
         each_weekday = each_holiday.begin.weekday()
+
+        assert each_holiday.duration == datetime.timedelta(days=1)
 
         if time_start is None and time_end is None:
             free_weekdays[each_weekday][-1] += 1
@@ -64,12 +48,23 @@ def main():
     sax19 = "https://www.ferienwiki.de/exports/feiertage/2019/de/sachsen"
     sax20 = "https://www.ferienwiki.de/exports/feiertage/2020/de/sachsen"
 
+    bav19 = "https://www.ferienwiki.de/exports/feiertage/2019/de/bayern"
+    bav20 = "https://www.ferienwiki.de/exports/feiertage/2020/de/bayern"
+
     calendar_urls = sax19, sax20
+    # calendar_urls = bav19, bav20
 
     calendars = tuple(get_calendar_from_url(_url) for _url in calendar_urls)
     calendar_full = merge_events(*calendars)
 
-    free_days = most_free_days(calendar_full, time_start=datetime.date(2019, 10, 1), time_end=datetime.date(2020, 5, 1))
+    start_date = datetime.date(2019, 10, 1)
+    # end_date = datetime.date(2020, 5, 1)
+    end_date = datetime.date(2020, 12, 30)
+
+    # start_date = datetime.date(2018, 3, 1)
+    # end_date = datetime.date(2019, 10, 1)
+
+    free_days = most_free_days(calendar_full, time_start=start_date, time_end=end_date)
     print("\n".join(f"{_x[0]:>10s}\t{_x[1]:>3d}" for _x in free_days))
 
 
